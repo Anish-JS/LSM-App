5597e4c6a4a8ad85299a6f53af270a34c452892a
AnishJakka <221910320002@gitam.in>
2024-07-15 11:01:15 -0400

    Added Chapter Edit - Access Settings - Video Upload

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormMessage,
+  FormControl,
+  FormDescription,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+import { cn } from "@/lib/utils";
+
+import { Chapter } from "@prisma/client";
+import { Editor } from "@/components/editor";
+import { Preview } from "@/components/preview";
+import { Checkbox } from "@/components/ui/checkbox";
+
+interface ChapterAccessFormProps {
+  initialData: Chapter;
+  courseId: string;
+  chapterId: string;
+}
+
+const formSchema = z.object({
+  isFree: z.boolean().default(false),
+});
+
+export const ChapterAccessForm = ({
+  initialData,
+  courseId,
+  chapterId,
+}: ChapterAccessFormProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { isFree: !!initialData.isFree },
+  });
+  const [isEditing, setIsEditing] = useState(false);
+  const toggleEdit = () => {
+    setIsEditing((current) => !current);
+  };
+  const router = useRouter();
+
+  const { isSubmitting, isValid } = form.formState;
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // console.log(values);
+    try {
+      await axios.patch(
+        `/api/courses/${courseId}/chapters/${chapterId}`,
+        values
+      );
+      toast.success("Chapter updated successfully");
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Chapter Access
+        <Button variant="ghost" onClick={toggleEdit}>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit Access
+            </>
+          )}
+        </Button>
+      </div>
+      <div>{/* <Checkbox /> */}</div>
+      {!isEditing && (
+        <p
+          className={cn(
+            "text-sm",
+            !initialData.isFree && "text-slate-500 italic"
+          )}
+        >
+          {initialData.isFree ? (
+            <>This chapter is free for preview</>
+          ) : (
+            <>This chapter is not free</>
+          )}
+        </p>
+      )}
+      {isEditing && (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="isFree"
+              render={({ field }) => {
+                return (
+                  <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <div className="space-y-1 leading-none">
+                      <FormDescription>
+                        Check this box if you want to make this chapter free for
+                        preview
+                      </FormDescription>
+                    </div>
+                  </FormItem>
+                );
+              }}
+            />
+            <div className="flex item-center gap-x-2 mt-2">
+              <Button disabled={isSubmitting || !isValid} type="submit">
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
--- app/api/courses/[courseId]/chapters/[chapterId]/route.tsx
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.tsx
@@ -1,13 +1,23 @@
+import Mux from "@mux/mux-node";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
+
+const Video = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID!,
+  tokenSecret: process.env.MUX_TOKEN_SECRET!,
+});
+
 export async function PATCH(
   req: Request,
   { params }: { params: { courseId: string; chapterId: string } }
 ) {
   try {
     const { userId } = auth();
-    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+    if (!userId) {
+      console.log("ERROR");
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
     const { isPublished, ...values } = await req.json();
 
     const ownCourse = await db.course.findUnique({
@@ -23,6 +33,31 @@ export async function PATCH(
       },
       data: { ...values },
     });
+
+    if (values.videoUrl) {
+      const exisitingMuxData = await db.muxData.findFirst({
+        where: { id: params.chapterId },
+      });
+      if (exisitingMuxData) {
+        await Video.delete(exisitingMuxData.assetId);
+        await db.muxData.delete({
+          where: { id: exisitingMuxData.id },
+        });
+      }
+      const asset = await Video.video.assets.create({
+        input: values.videoUrl,
+        playback_policy: ["public"],
+        test: false,
+      });
+
+      await db.muxData.create({
+        data: {
+          chapterId: params.chapterId,
+          assetId: asset.id,
+          playbackId: asset.playback_ids?.[0]?.id,
+        },
+      });
+    }
     //TODO: Handle Video upload
     return NextResponse.json(chapter);
   } catch (error) {
